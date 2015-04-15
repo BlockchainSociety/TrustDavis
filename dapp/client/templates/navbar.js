c7be593fd2f7b1f315129a150fc4ad540a42afe0
Joris Bontje <jorisbontje@gmail.com>
2015-04-15 16:56:28 +0200

    fake login mechanism

--- dapp/client/templates/navbar.js
+++ dapp/client/templates/navbar.js
@@ -3,5 +3,8 @@ Template.navbar.helpers({
       var currentRoute = Router.current();
       return currentRoute &&
         template === currentRoute.lookupTemplate() ? 'active' : '';
+    },
+    userId: function() {
+        return Meteor.connection.userId();
     }
 });
--- dapp/lib/constants.js
+++ dapp/lib/constants.js
@@ -1,3 +1,5 @@
 TRADE_TYPES = ["buy", "sell"];
 
 TRADE_STATUSES = ["new", "accepted", "cancelled", "insured", "partial_success", "completed", "failed", "expired"];
+
+TRUSTDAVIS_UID = "trustdavis:uid";
