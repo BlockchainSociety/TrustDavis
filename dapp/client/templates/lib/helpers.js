df76bb7ebb6eab9201f47b9ead0b6e08df3d090b
Joris Bontje <jorisbontje@gmail.com>
2015-04-16 13:00:26 +0200

    min width for progrss bar and humanize valid until

--- dapp/client/templates/lib/helpers.js
+++ dapp/client/templates/lib/helpers.js
@@ -1,3 +1,7 @@
 Template.registerHelper("formatDate", function(date) {
     return moment(date).format('YYYY-MM-DD');
 });
+
+Template.registerHelper("formatDateFromNow", function(date) {
+    return moment(date).fromNow();
+});
