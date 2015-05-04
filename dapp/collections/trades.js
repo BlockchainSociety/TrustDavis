94ddfd34c4fa0f1f61e259c567a1578605b08607
Joris Bontje <jorisbontje@gmail.com>
2015-05-04 16:28:35 +0100

    flip buyer and seller pct

--- dapp/collections/trades.js
+++ dapp/collections/trades.js
@@ -80,7 +80,7 @@ Trades.helpers({
     buyerInsurance: function() {
         var total = 0;
 
-        var buyerReferences = References.find({tradeId: this._id, objectId: this.sellerId});
+        var buyerReferences = References.find({tradeId: this._id, objectId: this.buyerId});
         buyerReferences.forEach(function(reference) {
             total += reference.amount;
         });
@@ -90,7 +90,7 @@ Trades.helpers({
     sellerInsurance: function() {
         var total = 0;
 
-        var sellerReferences = References.find({tradeId: this._id, objectId: this.buyerId});
+        var sellerReferences = References.find({tradeId: this._id, objectId: this.sellerId});
         sellerReferences.forEach(function(reference) {
             total += reference.amount;
         });
